cb1c291425108dc43c183cc637f71c3c1a2d4510
techTempleSem <techTempleSem@gmail.com>
2024-03-22 21:59:05 +0900

    error api

--- src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
+++ src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.example.exception.exception;
 
+import com.example.exception.model.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 @RestControllerAdvice(basePackages = "com.example.exception.controller")
@@ -19,5 +23,20 @@ public class RestApiExceptionHandler {
     public ResponseEntity outOfBound(IndexOutOfBoundsException e){
         log.error("OutOfIndexException : {}",e);
         return ResponseEntity.status(200).build();
+    }
+
+    @ExceptionHandler(value = {NoSuchElementException.class})
+    public ResponseEntity<Api> noSuchElement(
+            NoSuchElementException e
+    ){
+        log.error("",e);
+
+        var response = Api.builder()
+                .resultCode(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                .resultMessage(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
     }
 }
